5115e5042baf3b56736c9994e53a4eb47dbb8a45
huywang254 <killer23street@gmail.com>
2024-10-14 09:47:50 +0700

    14_10_7

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,21 +3,30 @@ import { currentUser } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'; // Đây là phần thêm để chỉ định route động
+
 export async function GET() {
   try {
     const clerkUser = await currentUser();
 
-    const user = await prisma.user.findUnique({
-        where: {
-            clerkUserId: clerkUser?.id
-          }
-      });
+    if (!clerkUser) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
 
-    // Return the topics as JSON
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkUserId: clerkUser?.id
+      }
+    });
+
+    if (!user) {
+      return new NextResponse('User not found', { status: 404 });
+    }
+
+    // Return the user as JSON
     return NextResponse.json({ user });
   } catch (error) {
-    console.error('Error at /api/topics/', error);
+    console.error('Error at /api/user/', error);
     return new NextResponse('Internal Server Error', { status: 500 });
   }
 }
-
