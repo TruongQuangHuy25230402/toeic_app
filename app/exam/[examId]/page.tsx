7d0f568288b34343ce966181b91d594b542a6e8b
huywang254 <killer23street@gmail.com>
2024-10-19 11:24:17 +0700

    update_19_10_1

--- app/exam/[examId]/page.tsx
+++ app/exam/[examId]/page.tsx
@@ -1,5 +1,6 @@
 import { getExamById } from '@/actions/getExamById'
 import { getPart5 } from '@/actions/getPart5'
+import {getPart1} from '@/actions/getPart1'
 import AddToeicExamForm from '@/components/exam/AddToeicExamForm'
 import React from 'react'
 
@@ -12,9 +13,10 @@ interface ExamPageProps {
 const Exam = async({params} : ExamPageProps) => {
     const exam = await getExamById(params.examId); // tại actions getExamById.ts
     const part5s = await getPart5(params.examId);
+    const part1s = await getPart1(params.examId)
   return (
     <div>
-      <AddToeicExamForm exam={exam} part5={part5s || []} />
+      <AddToeicExamForm exam={exam} part1={part1s || []} part5={part5s || []} />
     </div>
   )
 }
