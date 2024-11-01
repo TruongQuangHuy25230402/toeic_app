3de79f97f3f6046c2a73b3321a28a2db6f4477c9
huywang254 <killer23street@gmail.com>
2024-11-01 20:16:57 +0700

    1_11_8

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -51,7 +51,7 @@ const CourseLayout = async ({
         <CourseNavbar course={course} progressCount={progressCount} />
       </div>
       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
-        {/* @ts-expect-error Async Server Component */}
+        
         <CourseSidebar course={course} progressCount={progressCount} />
       </div>
       <main className="md:pl-80 pt-[80px] h-full ">{children}</main>
