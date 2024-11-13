5aeede27a9f5eeeefdd3d3d991818d99a888160f
huywang254 <killer23street@gmail.com>
2024-11-13 14:28:29 +0700

    Added new feature or fixed bug_1

--- app/api/examQuestion/[id]/route.ts
+++ app/api/examQuestion/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export default async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(req: Request, { params }: { params: { id: string } }) {
   const { id: questionId } = params; // Extract the questionId from URL params
 
   try {
